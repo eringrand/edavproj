049815ba57ada60cbec658a463641857ae6aa2be
Stephen Ra <stephenrra@gmail.com>
2015-04-12 20:33:02 -0400

    Added join script

---
+++ data/join.R
@@ -0,0 +1,9 @@
+setwd("/home/ra/edavproj/data/")
+
+
+hs = read.csv("DOE_High_School_Directory_2014-2015.csv", header=T, stringsAsFactors = F)
+sat <- read.csv("2014SATWebsite10214.csv", header=T, stringsAsFactors = F)
+
+require(dplyr)
+
+hsSAT <- left_join(hs, sat)
