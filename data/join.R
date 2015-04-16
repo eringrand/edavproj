ee81958ea249ce01ab25e1d77fd6a6a21343561d
eringrand <eringrand@gmail.com>
2015-04-15 22:17:02 -0400

    joined correctly

--- data/join.R
+++ data/join.R
@@ -6,8 +6,9 @@ sat <- read.csv("2014SATWebsite10214.csv", header=T, stringsAsFactors = F)
 
 require(dplyr)
 
-hsSAT <- left_join(hs, sat, by="dbn")
+hsSAT <- left_join(sat, hs, by="dbn")
 
 hsSAT <- select(hsSAT, -boro, -High.School, -se_services, -ell_programs, -school_accessibility_description, -number_programs)
 
 write.csv(hsSAT, file = "data.csv", row.names=FALSE, quote=FALSE)
+
