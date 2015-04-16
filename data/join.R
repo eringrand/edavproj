2406fc190e3d758a3bc140bcc26227581df52c86
eringrand <eringrand@gmail.com>
2015-04-15 21:38:01 -0400

    editing!

--- data/join.R
+++ data/join.R
@@ -1,9 +1,9 @@
 setwd("~/edavproj/data/")
 
 
-hs = read.csv("DOE_High_School_Directory_2014-2015.csv", header=T, stringsAsFactors = F)
+hs <- read.csv("DOE_High_School_Directory_2014-2015.csv", header=T, stringsAsFactors = F)
 sat <- read.csv("2014SATWebsite10214.csv", header=T, stringsAsFactors = F)
 
 require(dplyr)
 
-hsSAT <- left_join(hs, sat)
+hsSAT <- left_join(hs, sat, by=dbn)
