65c7b99715c3abda607e4db9e93662980541bb0e
eringrand <eringrand@gmail.com>
2015-04-15 23:14:39 -0400

    joined safety info

--- data/join.R
+++ data/join.R
@@ -2,11 +2,16 @@ setwd("~/edavproj/data/")
 require(dplyr)
 
 hs <- read.csv("DOE_High_School_Directory_2014-2015.csv", header=T, stringsAsFactors = F)
-sat <- read.csv("2014SATWebsite10214.csv", header=T, stringsAsFactors = F)
+hs <- select(hs, -boro, -se_services, -ell_programs,  -school_accessibility_description, -number_programs)
 
+sat <- read.csv("2014SATWebsite10214.csv", header=T, stringsAsFactors = F)
+sat <- select(sat, -High.School)
+
+safety <- read.csv("School_Safety_Report.csv", header=T, stringsAsFactors = F)
+names(safety) <- tolower(names(safety))
+safety <- select(safety, -address, -borough, -building.name, -schools.in.building, -building.code, -id)
 
 hsSAT <- left_join(sat, hs, by="dbn")
+join <- left_join(sat, safety, by=c("dbn"))
 
-hsSAT <- select(hsSAT, -boro, -High.School, -se_services, -ell_programs, -school_accessibility_description, -number_programs, -Location.1)
-write.csv(hsSAT, file = "data.csv", row.names=FALSE, quote=FALSE)
 
