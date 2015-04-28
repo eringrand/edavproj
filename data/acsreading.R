4e3c78cd27fda45b7775a7526a3e0ba546b50c89
eringrand <eringrand@gmail.com>
2015-04-27 21:11:22 -0400

    edits

--- data/acsreading.R
+++ data/acsreading.R
@@ -14,8 +14,7 @@ xnew <- mutate(xnew, names=rownames(xnew))
 x2 <- xnew
 #x2 <- filter(xnew, xnew$X1 != "Percent")
 x2 <- filter(x2, x2$X1 != "Percent MOE")
-xnew2 <- x2[-4,]
-xsel <- select(xnew2, names, X1, X6, X7, X27, X28, X29, X30, X31, X32, X33, X34, X35, X36, X37, X12)
+xsel <- select(x2, names, X1, X6, X7, X27, X28, X29, X30, X31, X32, X33, X34, X35, X36, X37, X12)
 colnames <- c("Neighborhood", "X1", "Employed",   "Unemployed", "Total households",  
               "Less than $10,000", "$10,000 to $14,999",  "$15,000 to $24,999",  
               "$25,000 to $34,999",  "$35,000 to $49,999", "$50,000 to $74,999",  
@@ -40,6 +39,6 @@ xsel$Unemployed <- as.numeric(xsel$Unemployed)
 xsel$Employed <- as.numeric(xsel$Employed)
 xsel <- xsel %>% mutate(fracunemployed = Unemployed / Employed)
 
-
-final <- select(xsel, ID, X1, fracunemployed, Unemployment.Rate)
+final <- xsel
+#final <- select(xsel, ID, X1, fracunemployed, Unemployment.Rate)
 
